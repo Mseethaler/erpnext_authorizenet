89f52e010719a6ce80f56962244f0e0e350ade0e
Mseethaler <michaels@nc.rr.com>
2026-03-31 20:42:35 -0400

    Use on_update instead of validate to register gateway after doc is saved

--- erpnext_authorizenet/authorize_net_gateway/doctype/authorize_net_settings/authorize_net_settings.py
+++ erpnext_authorizenet/authorize_net_gateway/doctype/authorize_net_settings/authorize_net_settings.py
@@ -39,7 +39,7 @@ SUPPORTED_CURRENCIES = [
 class AuthorizeNetSettings(frappe.model.document.Document):
 
 	def validate(self):
-		"""Called on Save. Registers this gateway instance with the Frappe payments system."""
+		"""Called after Save. Registers this gateway instance with the Frappe payments system."""
 		self._register_gateway()
 		call_hook_method("payment_gateway_enabled", gateway=f"Authorize.Net-{self.gateway_name}")
 
